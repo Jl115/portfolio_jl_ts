dc8b9d8d7db856de2af0ac1ec0cdcc49db96be77
Jl115 <joelbern006@gmail.com>
2024-08-25 13:47:32 +0200

    fix: update build files for hostinger

---
+++ assets/ProfileView-BbE7VJf0.js
@@ -0,0 +1,1 @@
+import{d as e,_ as o,o as t,c as n}from"./index-CcPvmNy_.js";const r=e({components:{},setup(){return{}}});function s(c,p,a,i,f,_){return t(),n("div",null,"hi im the profile")}const d=o(r,[["render",s]]);export{d as default};
